a8b42f59cd8d5ec58dc370d5601f12b01d88186c
TechBingeWorld <ahmad.arman.deos@gmail.com>
2024-03-23 15:12:53 +0530

    product controller and all type of error handling done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import dotenv, { config } from "dotenv";
 dotenv.config({ path: "./env" });
 import connectDB from "./db/connection.js";
 
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log("Shutting down the server due to Uncaught Exception");
+  process.exit(1);
+});
+
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8080, () => {
@@ -12,3 +19,10 @@ connectDB()
   .catch((error) => {
     console.log("MongoDB connection FAILED !", error);
   });
+
+// Unhandeled Rejection --
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Error : ${err.message}`);
+//   console.log(`Shutting down the server due to Unhandled Rejection`);
+//   server.close(() => process.exit(1));
+// });
